62974959b4425fbfb0442314665343c0cfe89615
Salar Rahmanian <code@softinio.com>
2023-02-11 15:30:04 -0800

    lint

--- lua/plugins/mind.lua
+++ lua/plugins/mind.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     require("mind").setup({
       persistence = {
-            dataDir = "~/Documents/mind.nvim/data";
-            statePath = "~/Documents/mind.nvim/mind.json";
+        dataDir = "~/Documents/mind.nvim/data",
+        statePath = "~/Documents/mind.nvim/mind.json",
       },
     })
   end,
