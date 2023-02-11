9431d011d164289f0f16b851dd8605a0bf334c46
Salar Rahmanian <code@softinio.com>
2023-02-11 14:23:51 -0800

    add persistence locations for mind.nvim

--- lua/plugins/mind.lua
+++ lua/plugins/mind.lua
@@ -2,6 +2,11 @@ return {
   "phaazon/mind.nvim",
   branch = "v2.2",
   config = function()
-    require("mind").setup()
+    require("mind").setup({
+      persistence = {
+            dataDir = "~/Documents/mind.nvim/data";
+            statePath = "~/Documents/mind.nvim/mind.json";
+      },
+    })
   end,
 }
