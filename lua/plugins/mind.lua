a8853c25bb1c257ee6f0e6d5ba9d317fbdde6b7f
Salar Rahmanian <code@softinio.com>
2023-02-23 17:55:09 -0800

    add key bindings for mind

--- lua/plugins/mind.lua
+++ lua/plugins/mind.lua
@@ -1,6 +1,13 @@
 return {
   "phaazon/mind.nvim",
   branch = "v2.2",
+  keys = {
+    { "<leader>nm", "<cmd>MindOpenMain<cr>", desc = "Open Main Mind" },
+    { "<leader>np", "<cmd>MindOpenProject<cr>", desc = "Open Mind for current project" },
+    { "<leader>ns", "<cmd>MindOpenSmartProject<cr>", desc = "Open Mind for smart project" },
+    { "<leader>nc", "<cmd>MindClose<cr>", desc = "Close Mind" },
+    { "<leader>nr", "<cmd>MindReloadState<cr>", desc = "Mind: Reload State" },
+  },
   config = function()
     require("mind").setup({
       persistence = {
