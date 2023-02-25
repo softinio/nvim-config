8b3d267492557f013cf2a65c92fd3e4f1b83b49d
Salar Rahmanian <code@softinio.com>
2023-02-24 19:09:30 -0800

    fix composer

--- lua/plugins/markdown-composer.lua
+++ lua/plugins/markdown-composer.lua
@@ -1,8 +1,5 @@
 return {
   "euclio/vim-markdown-composer",
-  keys = {
-    { "<leader>nv", "<cmd>ComposerStart<cr>", desc = "Composer Start" },
-  },
-  build = "cargo build --release --locked"
+  build = "cargo build --release"
 }
 
