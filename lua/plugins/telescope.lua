e2ac81a6560332af21e18a63482965d68dc299bc
Salar Rahmanian <code@softinio.com>
2023-02-28 18:14:01 -0800

    undo keymap correction

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,6 +38,6 @@ return {
     vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
     vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-    vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo", { desc = "[S]earch [U]ndo" })
+    vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<cr>", { desc = "[S]earch [U]ndo" })
   end,
 }
