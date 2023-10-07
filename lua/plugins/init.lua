c2d6d6379dba1c85381e8f18ff5bd1127255306c
Salar Rahmanian <code@softinio.com>
2023-10-06 22:11:34 -0700

    switch back to dark theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,7 +8,7 @@ return {
   },
   {
     "folke/tokyonight.nvim",
-    lazy = true, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       vim.g.tokyonight_style = "night"
@@ -19,8 +19,8 @@ return {
     end,
   },
   { 
-    "ellisonleao/gruvbox.nvim", 
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    "ellisonleao/gruvbox.nvim",
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000 , 
     config = function()
       require("gruvbox").setup({
