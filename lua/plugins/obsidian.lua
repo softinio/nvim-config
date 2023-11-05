a2a4948626b84eb5115d2826afc76978c659e535
Salar Rahmanian <code@softinio.com>
2023-11-05 14:58:59 -0800

    obsidian plugin updates

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -8,6 +8,12 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  keys = {
+    { "<leader>no", "<cmd>ObsidianQuickSwitch<cr>", desc = "Obsidian Quick Switch (Telescope)" },
+    { "<leader>nn", "<cmd>ObsidianNew<cr>", desc = "Create a new obsidian note" },
+    { "<leader>ns", "<cmd>ObsidianSearch<cr>", desc = "Obsidian Search" },
+    { "<leader>not", "<cmd>ObsidianToday<cr>", desc = "Obsidian new daily note" },
+  },
   opts = {
     workspaces = {
       {
