d1f3552a6bb1b574f553301a99482652be6ab5f3
Salar Rahmanian <code@softinio.com>
2023-11-05 20:26:08 -0800

    better neorg key mappings

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -4,10 +4,10 @@ return {
   build = ":Neorg sync-parsers",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    { "<leader>ni", "<cmd>Neorg index<cr>", desc = "Index" },
-    { "<leader>ns", "<cmd>Neorg generate-workspace-summary<cr>", desc = "Generate Workspace Summary" },
-    { "<leader>njt", "<cmd>Neorg journal today<cr>", desc = "Journal Today" },
-    { "<leader>njT", "<cmd>Neorg journal template<cr>", desc = "Journal Template" },
+    { "<leader>ni", "<cmd>Neorg index<cr>", desc = "Neorg index" },
+    { "<leader>nw", "<cmd>Neorg generate-workspace-summary<cr>", desc = "Neorg generate Workspace Summary" },
+    { "<leader>njt", "<cmd>Neorg journal today<cr>", desc = "Neorg Journal Today" },
+    { "<leader>njT", "<cmd>Neorg journal template<cr>", desc = "Neorg Journal Template" },
   },
   config = function()
       require("neorg").setup {
