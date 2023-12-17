0c64477fde776a16cb57fdd5525df5008026c771
Salar Rahmanian <code@softinio.com>
2023-12-17 13:55:43 -0800

    remap tab for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,9 @@
 return {
   "github/copilot.vim",
   lazy = false,
+  config = function ()
+    vim.api.nvim_set_keymap('i', '<C-Tab>', 'copilot#Accept("<End>")', { silent = true, script = true, expr = true })
+    vim.g.copilot_no_tab_map = true;
+    vim.g.copilot_assume_mapped = true;
+  end
 }
