03bf0a72b1c59822d0cc07b5c05ac1e2e724a0df
Salar Rahmanian <code@softinio.com>
2024-09-23 18:34:30 -0700

    yank highlight tweak

--- init.lua
+++ init.lua
@@ -79,10 +79,10 @@ else
 
   -- [[ Highlight on yank ]]
   -- See `:help vim.highlight.on_yank()`
-  local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+  local highlight_group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
   vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 1000 })
+      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
     end,
     group = highlight_group,
     pattern = "*",
