2630d96e0b932baaef3430407ac9a37879670ddf
Salar Rahmanian <code@softinio.com>
2024-09-23 18:15:26 -0700

    improvements to yank

--- init.lua
+++ init.lua
@@ -82,7 +82,7 @@ else
   local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
   vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-      vim.highlight.on_yank()
+      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 1000 })
     end,
     group = highlight_group,
     pattern = "*",
