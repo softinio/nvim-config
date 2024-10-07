93702f8d19a26f801a1bbf4402f976a3e2c67103
Salar Rahmanian <code@softinio.com>
2024-10-06 23:35:50 -0700

    use expand

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,7 +6,7 @@ return {
   opts = {
     provider = "openai",
     openai = {
-      api_key_name = "cmd:cat /Users/salar/.openai",
+      api_key_name = "cmd:cat" .. vim.fn.expand("~/.openai"),
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
