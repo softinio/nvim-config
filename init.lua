f962e982b648af6090a3f491996efcfd2cc37ee1
Salar Rahmanian <code@softinio.com>
2025-02-28 13:48:33 -0800

    try new highlight yank setting

--- init.lua
+++ init.lua
@@ -82,7 +82,7 @@ else
   local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
   vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
-      vim.highlight.on_yank()
+      vim.highlight.on_yank({ higroup = "Search" })
     end,
     group = highlight_group,
     pattern = "*",
